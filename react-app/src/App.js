1ec8d068b160ab4926b89c8d09a682d051855885
KevinH48264 <1kevin.huang@gmail.com>
2023-01-30 22:26:10 -0500

    working version for just adding URLs or text

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,13 +6,18 @@ function App() {
   const [text, setText] = useState('');
   const [query, setQuery] = useState('');
   const [answer, setAnswer] = useState('');
+  const [status, setStatus] = useState('Waiting for submit button to be pressed');
+  const [modelIndex, setModelIndex] = useState(Math.floor(Math.random() * (100 - 1) + 1))
+  const [openAIKey, setOpenAIKey] = useState('')
   const URL = 'https://opendoc-conirvxfeq-uc.a.run.app'
 
 
   function embedClick() {
     console.log(text);
+    setStatus("Loading")
     // todo: update model_index so it's not always 0
-    const data = { model_index: 0, text: text };
+    setModelIndex(Math.floor(Math.random() * (100 - 1) + 1))
+    const data = { model_index: modelIndex, text: text, openai_key: openAIKey };
 
     const options = {
       method: 'POST',
@@ -25,19 +30,15 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
         console.log("RESULT HERE: ", data);
+        setStatus("Ready for you to ask a question!")
     });
   }
 
-//   {
-//     "model_index" : 0,
-//     "prompt" : "what are the main takeaway?",
-//     "type" : "text"
-// }
-
 function queryClick() {
   console.log(query);
+  setAnswer("Response: Loading")
   // todo: update model_index so it's not always 0
-  const data = { model_index: 0, prompt: query, type: "text" };
+  const data = { model_index: modelIndex, prompt: query, type: "text" };
 
   const options = {
     method: 'POST',
@@ -52,7 +53,7 @@ function queryClick() {
       console.log("QUERY RESPONSE HERE: ", data);
       console.log(data["Response"])
       console.log(data["Response"]["response"])
-      setAnswer(data["Response"]["response"])
+      setAnswer("Response: " + data["Response"]["response"])
   });
 }
 
@@ -60,11 +61,17 @@ function queryClick() {
   return (
     <div className="App" style={{ display: "flex", flexDirection: "row"}}>
       <div style={{ display: "flex", flexDirection: "column", padding: "50px", width: "50%"}}>
+        <p>Enter text or a URL you'd like to ask questions about</p>
         <textarea style={{height: "50vh"}} onChange={(e) => setText(e.target.value)} />
+        <div style={{display: "flex", alignItems: "center"}}>
+          <p>Use OpenAI API Key [optional, but better results]:</p>
+          <textarea style={{height: "30px", marginLeft: "10px" }} onChange={(e) => setOpenAIKey(e.target.value)} />
+        </div>
         <button style={{ height: "50px", marginTop: "15px"}} onClick={embedClick}>Submit</button>
       </div>
       <div style={{ display: "flex", flexDirection: "column", padding: "50px", width: "50%"}}>
-        <textarea style={{height: "50vh"}} onChange={(e) => setQuery(e.target.value)} />
+        <p>Status: {status}</p>
+        <textarea style={{height: "25vh"}} onChange={(e) => setQuery(e.target.value)} />
         <button style={{ height: "50px", marginTop: "15px"}} onClick={queryClick}>Ask Question</button>
         <div>
           <p>{answer}</p>
