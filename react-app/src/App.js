ba966717519d7e01ae6c09dc9fdfa2ad272fc69a
KevinH48264 <1kevin.huang@gmail.com>
2023-03-09 21:46:54 -0500

    update

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { createParser } from 'eventsource-parser'
 
 function App() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState('TEMPORARY TEXT');
   const [query, setQuery] = useState('');
   const [answer, setAnswer] = useState('');
   const [status, setStatus] = useState('Waiting for submit button to be pressed');
@@ -58,8 +58,78 @@ function queryClick() {
 }
 
 
+  // for testing the chatgpt server
+  useEffect(() => {
+    // TODO: Edit this as necessary
+    let data = {
+        website_text: "",
+        prompt: "hi",
+        chat_history: []
+    };
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      mode: 'cors',
+      body: JSON.stringify(data)
+    };
+
+    // TODO: Edit this URL
+    fetch(`https://opendoc-conirvxfeq-uc.a.run.app/chat_stream`, options).then(async (response) => {
+      console.log("Calling OpenAI API")
+      var newText = ""
+
+      // TODO: Edit setText accordingly
+      setText("")
+
+      // checks for errors
+      if (!response.ok) {
+        const error = await response.json().catch(() => ({}))
+        throw new Error((error) ? JSON.stringify(error) : `${response.status} ${response.statusText}`)
+      }
+
+      // helper function to parse through each stream event and update new text
+      const parser = createParser((event) => {
+        if (event.type === 'event') {
+          if ((event.data !== '[DONE]')) {
+            var eventData = JSON.parse(event.data)
+            if ('content' in eventData.choices[0].delta) {
+              newText += eventData['choices'][0]["delta"]["content"]
+
+              // TODO: Edit setText accordingly
+              setText(newText)
+            }
+          }
+        }
+      })
+
+      // catches chunks from stream
+      for await (const chunk of streamAsyncIterable(response.body)) {
+        const event = new TextDecoder().decode(chunk)
+        parser.feed(event)
+      }
+    })
+
+    // helper function to read from stream until done
+    async function* streamAsyncIterable(stream) {
+      const reader = stream.getReader()
+      try {
+        while (true) {
+          const { done, value } = await reader.read()
+          if (done) {
+            return
+          }
+          yield value
+        }
+      } finally {
+        reader.releaseLock()
+      }
+    }
+  }, [])
+  
+
   return (
     <div className="App" style={{ display: "flex", flexDirection: "row"}}>
+      <p>{text}</p>
       <div style={{ display: "flex", flexDirection: "column", padding: "50px", width: "50%"}}>
         <p>Enter text or a URL you'd like to ask questions about</p>
         <textarea style={{height: "50vh"}} onChange={(e) => setText(e.target.value)} />
