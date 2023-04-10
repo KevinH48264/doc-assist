d62351c9e1f96780f03fbf2a143c33be14858ce4
KevinH48264 <1kevin.huang@gmail.com>
2023-04-09 21:43:11 -0400

    tried adding entire pdf viewer, didn't work because v2 to v3 manifest transition was not loading

---
+++ extension/public/background.js
@@ -0,0 +1,8 @@
+importScripts("options/migration.js");
+importScripts("preserve-referer.js");
+importScripts("pdfHandler.js");
+importScripts("extension-router.js");
+importScripts("pdfHandler-vcros.js");
+importScripts("pageAction/background.js");
+importScripts("suppress-update.js");
+importScripts("telemetry.js");
